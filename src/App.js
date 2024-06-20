f5d226fe7c3aa2350bdc0190fda689d344e33c81
Nitish Rawat <nitishrawat009@gmail.com>
2024-06-20 17:57:45 +0530

    Restarting according to React docs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-// can omit .js from Greet.js location
-import Greet from './Components/Greet'
-// import Square from './Components/Square'
-import Board from './Components/Board'
-
-function App() {
-  return (
-    <>
-      {/* <Greet/> */}
-      <Board/>
-    </>
-  );
+export default function Square() {
+  return <button className="square">X</button>;
 }
-
-export default App;
--- src/Components/Greet.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-// es6 based format
-const Greet = () => <h1>Nitish ka function based component</h1>
-
-export default Greet
